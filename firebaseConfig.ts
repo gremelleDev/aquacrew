7321035445e09b651b0027880b0103a745c3e2b6
aquacrew.app <aquacrew.app@gmail.com>
2025-07-05 09:52:43 +0000

    feat: Complete project setup and configuration

---
+++ firebaseConfig.ts
@@ -0,0 +1,12 @@
+// firebaseConfig.ts
+import { FirebaseOptions } from '@react-native-firebase/app';
+
+export const firebaseConfig: FirebaseOptions = {
+    apiKey: "AIzaSyCkFVm2ipWJkz1DZ77q9lbr3qjQ7vwTvY8",
+    authDomain: "aquacrew-9ff87.firebaseapp.com",
+    projectId: "aquacrew-9ff87",
+    storageBucket: "aquacrew-9ff87.firebasestorage.app",
+    messagingSenderId: "523774459710",
+    appId: "1:523774459710:web:b7a52c706d470fe3f8b66f",
+    measurementId: "G-XRSZVFE5WM"
+};
